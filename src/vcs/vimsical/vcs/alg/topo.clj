47db3471d11c94cc47249db380cd3ab4012bfb79
Julien Fantin <julienfantin@gmail.com>
2017-04-20 18:31:43 -0400

    Import topo algs for uuid-based deltas

--- src/vcs/vimsical/vcs/alg/topo.clj
+++ src/vcs/vimsical/vcs/alg/topo.clj
@@ -9,44 +9,19 @@
    [vimsical.vcs.delta :as delta]))
 
 
-;; * Specs
-
-(defn ^:declared sorted? [branches deltas])
-(s/def ::deltas (s/every ::delta/delta))
-(s/def ::sorted (s/and ::deltas sorted?))
-
-
-;; * Topo sort
-
-(s/fdef sort
-        ;; NOTE Can't spec args without a valid delta seq gen
-        ;; :args (s/cat :branches ::branch/branch :deltas ::deltas)
-        :ret  ::sorted)
-
 (defn sort
   "Sort `deltas` in topological order, running time is linear to input size.
-
   The sort guarantees that no delta will appear before its previous delta.
-
-  `branches` should be a sequence of all branches, *fully denormalized*, and is
-  used to disambiguate when there are multiple deltas for the same :prev-id
-  value in the sequence. In those cases the logic is to designate as the
-  previous delta the one whose branch is equal to the current delta or is the
-  current delta's parent branch.
-
   NOTE even though the sort is deterministic for any given input, the output is
   dependent on the ordering of the input, so for example
-
   (let [s1 (shuffle my-delta-seq)
         s2 (shuffle my-delta-seq)
-        s1-sorted (sort my-branches s1)
-        s2-sorted (sort my-branches s2)]
+        s1-sorted (topo-sort s1)
+        s2-sorted (topo-sort s2)]
     (= s1-sorted s2-sorted))
-
   ...could be true, or false.
-
-  However `sorted?` returns true for both."
-  [branches deltas]
+  However `topo-sorted?` returns true for both."
+  [deltas]
   ;;
   ;; Implementation notes:
   ;;
@@ -58,48 +33,26 @@
   ;; CLJS transients don't implement iFn
   ;; http://dev.clojure.org/jira/browse/CLJS-1743
   ;;
-  (let [branches-by-id       (coll/index-by :db/id branches)
-        by-id                (group-by :id deltas)
-        by-prev-id           (group-by :prev-id deltas)
-        ;; Should be safe to memoize this since we'll always have a relatively
-        ;; small number of branches compared to the number of deltas
-        memoized-in-lineage? (memoize branch/in-lineage?)
-        delta->compound-key  (juxt :id :branch-id)]
-    (letfn [(delta->branch [{:keys [branch-id]}]
-              (get branches-by-id branch-id))
-            (delta-in-lineage? [delta delta?]
-              (memoized-in-lineage?
-               (delta->branch delta)
-               (delta->branch delta?)))
-            ;; Optimization to initialize the loop: we can find the last
-            ;; deltas without iterating if we consider that they're the ones
-            ;; that no other deltas point to
-            (last-deltas [by-id by-prev-id]
-              (mapcat
-               #(get by-id %)
-               (set/difference
-                (set (keys by-id))
-                (set (keys by-prev-id)))))
-            (next-deltas [by-prev-id! {:keys [id] :as delta}]
-              (seq
-               (filter
-                (fn [next-delta]
-                  (delta-in-lineage? next-delta delta))
-                (get by-prev-id! id))))
-            (prev-delta [by-id! {:keys [prev-id] :as delta}]
-              (first
-               (filter
-                (fn [prev-delta]
-                  (delta-in-lineage? delta prev-delta))
-                (get by-id! prev-id))))
-            (dissoc!-delta-by [f m! {:keys [id] :as delta}]
-              (let [pred (=by delta->compound-key delta)
-                    k    (f delta)
-                    v    (get m! k)
+  (let [by-id      (coll/index-by :id deltas)
+        by-prev-id (group-by :prev-id deltas)]
+    (letfn [(last-deltas [by-id by-prev-id]
+              (mapv #(get by-id %)
+                    (set/difference
+                     (set (keys by-id))
+                     (set (keys by-prev-id)))))
+            (next-deltas [by-prev-id {:keys [id]}]
+              (get by-prev-id id))
+            (prev-delta [by-id {:keys [prev-id]}]
+              (get by-id prev-id))
+            (dissoc!-delta [by-id! {:keys [id] :as delta}]
+              (dissoc! by-id! id))
+            (dissoc!-prev-delta [m! {:keys [prev-id] :as delta}]
+              (let [pred #(=by :id delta %)
+                    v    (get m! prev-id)
                     v'   (remove pred v)]
                 (if (seq v')
-                  (assoc! m! k v')
-                  (dissoc! m! k))))]
+                  (assoc! m! prev-id v')
+                  (dissoc! m! prev-id))))]
       (loop [by-id!         (transient by-id)
              by-prev-id!    (transient by-prev-id)
              ordered!       (transient [])
@@ -107,8 +60,8 @@
         (if (nil? delta)
           (reverse (persistent! ordered!))
           (let [prev-delta   (prev-delta by-id! delta)
-                by-id!'      (dissoc!-delta-by :id by-id! delta)
-                by-prev-id!' (dissoc!-delta-by :prev-id by-prev-id! delta)
+                by-id!'      (dissoc!-delta by-id! delta)
+                by-prev-id!' (dissoc!-prev-delta by-prev-id! delta)
                 more'        (if (nil? (next-deltas by-prev-id!' prev-delta))
                                (conj more prev-delta)
                                more)]
@@ -117,29 +70,22 @@
                    (conj! ordered! delta)
                    more')))))))
 
+
+
 (s/fdef sorted?
-        :args (s/cat :branches (s/every ::branch/branch) :deltas ::deltas)
+        :args (s/cat :deltas (s/every ::delta/delta))
         :ref  boolean?)
 
 (defn sorted?
-  [branches [delta & deltas]]
-  (let [branches-by-id (coll/index-by :db/id branches)]
-    (letfn [(delta->branch [{:keys [branch-id]}]
-              (branches-by-id branch-id))
-            (kfn [{:keys [id branch-id]}]
-              {:id id :branch-id branch-id})
-            (prev-kfn [{:keys [prev-id branch-id]}]
-              {:id prev-id :branch-id branch-id})
-            (pprev-kfn [{:keys [prev-id] :as delta}]
-              (let [parent-branch-id (-> delta delta->branch ::branch/parent :db/id)]
-                {:id prev-id :branch-id parent-branch-id}))
-            (seen? [ks-set delta]
-              (or (ks-set (prev-kfn delta))
-                  (ks-set (pprev-kfn delta))))]
-      (boolean
-       (reduce
-        (fn [ks-set delta]
-          (if (seen? ks-set delta)
-            (conj! ks-set (kfn delta))
-            (reduced false)))
-        (transient #{(kfn delta)}) deltas)))))
+  [[delta & deltas]]
+  (letfn [(kfn [{:keys [id]}] id)
+          (prev-kfn [{:keys [prev-id]}] prev-id)
+          (seen? [ks-set delta]
+            (ks-set (prev-kfn delta)))]
+    (boolean
+     (reduce
+      (fn [ks-set delta]
+        (if (seen? ks-set delta)
+          (conj! ks-set (kfn delta))
+          (reduced false)))
+      (transient #{(kfn delta)}) deltas))))
--- test/vcs/vimsical/vcs/alg/topo_test.clj
+++ test/vcs/vimsical/vcs/alg/topo_test.clj
@@ -10,76 +10,33 @@
 
 (deftest topo-test
   (testing "predicate across branches"
-    (let [master         (uuid)
-          child1         (uuid)
-          child11        (uuid)
-          child2         (uuid)
-          child21        (uuid)
-          file-id        (uuid)
-          id0            (uuid)
-          id1            (uuid)
-          id2            (uuid)
-          id3            (uuid)
-          id4            (uuid)
-          id5            (uuid)
-          master-branch  {::branch/name "master" :db/id master}
-          child1-branch  {::branch/name "child1" :db/id child1 ::branch/parent master-branch}
-          child11-branch {::branch/name "child11" :db/id child11 ::branch/parent child1-branch}
-          child2-branch  {::branch/name "child2" :db/id child2 ::branch/parent master-branch}
-          child21-branch {::branch/name "child21" :db/id child21 ::branch/parent child2-branch}
-          branches       [master-branch child1-branch child11-branch child2-branch child21-branch]
-          sorted
-          [{:branch-id master  :id id0 :prev-id nil :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id master  :id id1 :prev-id id0 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child1  :id id2 :prev-id id1 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child1  :id id3 :prev-id id2 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child2  :id id2 :prev-id id1 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child2  :id id3 :prev-id id2 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child21 :id id4 :prev-id id3 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child21 :id id5 :prev-id id4 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child11 :id id4 :prev-id id3 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child11 :id id5 :prev-id id4 :pad 0 :op [:str/ins nil "a"] :file-id file-id}]
-          nsorted
-          [{:branch-id master  :id id0 :prev-id nil :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id master  :id id1 :prev-id id0 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child1  :id id2 :prev-id id1 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child1  :id id3 :prev-id id2 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child21 :id id4 :prev-id id3 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child21 :id id5 :prev-id id4 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child2  :id id2 :prev-id id1 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child2  :id id3 :prev-id id2 :pad 0 :op [:str/ins nil "a"] :file-id file-id}]]
-      (is (sut/sorted? branches sorted))
-      (is (not (sut/sorted? branches nsorted)))))
+    (let [sorted
+          [{:branch-id (uuid :master)  :id (uuid :0) :prev-id nil        :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :master)  :id (uuid :1) :prev-id (uuid :0)  :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :child1)  :id (uuid :2) :prev-id (uuid :1)  :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :child1)  :id (uuid :3) :prev-id (uuid :2)  :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :child2)  :id (uuid :2) :prev-id (uuid :1)  :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :child2)  :id (uuid :3) :prev-id (uuid :2)  :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :child21) :id (uuid :4) :prev-id (uuid :3)  :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :child21) :id (uuid :5) :prev-id (uuid :4)  :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :child11) :id (uuid :4) :prev-id (uuid :3)  :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :child11) :id (uuid :5) :prev-id (uuid :4)  :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}]
+          nsorted (shuffle sorted)]
+      (is (sut/sorted? sorted))
+      (is (not (sut/sorted? nsorted)))))
   (testing "sort across branches"
-    (let [master         (uuid)
-          child1         (uuid)
-          child11        (uuid)
-          child2         (uuid)
-          child21        (uuid)
-          file-id        (uuid)
-          id0            (uuid)
-          id1            (uuid)
-          id2            (uuid)
-          id3            (uuid)
-          id4            (uuid)
-          id5            (uuid)
-          master-branch  {::branch/name "master" :db/id master}
-          child1-branch  {::branch/name "child1" :db/id child1 ::branch/parent master-branch}
-          child11-branch {::branch/name "child11" :db/id child11 ::branch/parent child1-branch}
-          child2-branch  {::branch/name "child2" :db/id child2 ::branch/parent master-branch}
-          child21-branch {::branch/name "child21" :db/id child21 ::branch/parent child2-branch}
-          branches       [master-branch child1-branch child11-branch child2-branch child21-branch]
-          deltas
-          [{:branch-id master  :id id0 :prev-id nil :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id master  :id id1 :prev-id id0 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child1  :id id2 :prev-id id1 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child1  :id id3 :prev-id id2 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child2  :id id2 :prev-id id1 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child2  :id id3 :prev-id id2 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child21 :id id4 :prev-id id3 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child21 :id id5 :prev-id id4 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child11 :id id4 :prev-id id3 :pad 0 :op [:str/ins nil "a"] :file-id file-id}
-           {:branch-id child11 :id id5 :prev-id id4 :pad 0 :op [:str/ins nil "a"] :file-id file-id}]
-          sorted-deltas  (sut/sort branches (shuffle deltas))]
-      (is (= (set deltas) (set sorted-deltas)))
-      (is (sut/sorted? branches sorted-deltas)))))
+    (let [deltas
+          [{:branch-id (uuid :master)  :id (uuid :0) :prev-id nil       :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :master)  :id (uuid :1) :prev-id (uuid :0) :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :child1)  :id (uuid :2) :prev-id (uuid :1) :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :child1)  :id (uuid :3) :prev-id (uuid :2) :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :child2)  :id (uuid :2) :prev-id (uuid :1) :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :child2)  :id (uuid :3) :prev-id (uuid :2) :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :child21) :id (uuid :4) :prev-id (uuid :3) :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :child21) :id (uuid :5) :prev-id (uuid :4) :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :child11) :id (uuid :4) :prev-id (uuid :3) :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}
+           {:branch-id (uuid :child11) :id (uuid :5) :prev-id (uuid :4) :pad 0 :op [:str/ins (uuid :n/a) "a"] :file-id (uuid :file)}]
+          nsorted (shuffle deltas)
+          sorted  (sut/sort (shuffle deltas))]
+      (is (sut/sorted? sorted))
+      (is (not (sut/sorted? nsorted))))))
