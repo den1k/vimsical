1afe56b3de44b4860e6818e3f5961052cad6f15f
den1k <kalavox@gmail.com>
2017-04-29 16:30:08 -0400

    Code-Editor: on cursor move, figure out position, scroll editor if needed, and focus it.

--- src/frontend/vimsical/frontend/code_editor/handlers.cljs
+++ src/frontend/vimsical/frontend/code_editor/handlers.cljs
@@ -4,32 +4,100 @@
    [vimsical.common.util.core :as util]
    [vimsical.frontend.vcs.subs :as vcs.subs]
    [vimsical.vcs.edit-event :as edit-event]
-   [vimsical.frontend.vcs.handlers :as vcs.handlers]))
+   [vimsical.frontend.vcs.handlers :as vcs.handlers]
+   [vimsical.vcs.file :as file]
+   [clojure.string :as str]))
+
+;;
+;; * Data Utils
+;;
+
+(defn ui-db->editor
+  ([ui-db reg-key file]
+   (ui-db->editor ui-db reg-key file :editor))
+  ([ui-db reg-key {:keys [db/id] :as file} k]
+   (let [editor (get-in ui-db [reg-key id])]
+     (case k
+       :editor editor
+       :model (.-model editor)))))
 
 ;;
 ;; * Monaco Utils
 ;;
+
+(defn pos->str-idx
+  "Takes a line, column, and lines, array of strings. Counts the characters of
+  each line, including \newline, returning an absolute positional index from
+  the beginning."
+  [line column lines]
+  (transduce (comp (take (dec line))
+                   (map #(inc (.-length %))))
+             +
+             (dec column)               ; dec bc we count from zero, monaco from one
+             lines))
+
+(defn idx->monaco-range
+  "Creates a monaco range object given either one position for a cursor or two
+  for a selection."
+  ([pos]
+   (idx->monaco-range pos pos))
+  ([start-pos end-pos]
+   {:pre [start-pos end-pos]}
+   #js {:startColumn     (:col start-pos)
+        :startLineNumber (:line start-pos)
+        :endColumn       (:col end-pos)
+        :endLineNumber   (:line end-pos)}))
+
+(defn idx->pos
+  "Takes an idx and a string and computes a :line and :col hashmap."
+  [idx string]
+  (let [str-len (count string)]
+    (cond
+      (zero? idx) {:line 1 :col 1}
+      (> idx str-len) nil               ; out of bounds
+      :else (reduce
+             (fn [[cur-idx l c :as step] char]
+               (let [next-idx (inc cur-idx)]
+                 (js/console.debug next-idx str-len)
+                 (cond
+                   (= cur-idx idx) (reduced {:line l :col c})
+                   ;; lookahead at last step of reduction
+                   ;; this becomes true only when idx is at last char
+                   (= next-idx str-len) (reduced {:line l :col (inc c)})
+                   :else (if (identical? \newline char)
+                           [next-idx (inc l) 1]
+                           [next-idx l (inc c)]))))
+             [0 1 1]
+             string))))
 
 (defn dispose-editor
   "Dispose Monaco editor"
   [editor]
   (.dispose editor))
 
-(defn update-editor-text [{:keys [db ui-db]} [_ reg-key {:keys [db/id] :as file}]]
-  (let [editor (get-in ui-db [reg-key id])
+(defn update-editor-text
+  [{:keys [db ui-db]} [_ reg-key {:keys [db/id] :as file}]]
+  (let [editor (ui-db->editor ui-db reg-key file)
         string (-> db
                    (vcs.subs/vims-vcs)
                    (vcs.subs/file-string file))]
-    (.setValue editor string)
-    nil))
+    (.setValue editor string))
+  nil)
 
-(defn pos->str-idx
-  ; Counts only until where we are
-  ; inc's at each line to account for \n
-  ; dec column because monaco returns col after event
-  [line column lines]
-  (transduce (comp (take (dec line))
-                   (map #(inc (.-length %)))) + (dec column) lines))
+(defn update-editor-cursor
+  [{:keys [db ui-db]} [_ reg-key {:keys [db/id] :as file}]]
+  (let [editor (ui-db->editor ui-db reg-key file)
+        ;; FIXME get cursor / sel idx from vcs
+        idx    2
+        string "12\n456\n89"
+        range  (idx->pos idx string)
+        rr     (idx->monaco-range range)]
+    (.revealRange editor rr)
+    (.setSelection editor rr)
+    (.focus editor))
+  nil)
+
+
 
 ;;
 ;; * Change Parsing
@@ -39,8 +107,8 @@
   (let [range         (.-range e)
         diff          (.-text e)
         added-count   (.-length diff)
-        deleted-count (.-rangeLength e)
         start-line    (.-startLineNumber range)
+        deleted-count (.-rangeLength e)
         start-column  (.-startColumn range)
         lines         (.getLinesContent model)]
     {:idx     (pos->str-idx start-line start-column lines)
@@ -87,16 +155,6 @@
       {::edit-event/op :crsr/sel ::edit-event/range [start-idx end-idx]})))
 
 ;;
-;; * Data Utils
-;;
-
-(defn ui-db->editor-info [ui-db reg-key {:keys [db/id] :as file}]
-  (let [editor (get-in ui-db [reg-key id])
-        model  (.-model editor)]
-    {::editor editor
-     ::model  model}))
-
-;;
 ;; * Handlers
 ;;
 
@@ -116,15 +174,25 @@
    {:ui-db (util/dissoc-in ui-db [reg-key id])}))
 
 (re-frame/reg-event-fx
- ::init
+ ::update-editor-text
  [(re-frame/inject-cofx :ui-db)]
  update-editor-text)
+
+(re-frame/reg-event-fx
+ ::update-editor-cursor
+ [(re-frame/inject-cofx :ui-db)]
+ update-editor-cursor)
+
+(re-frame/reg-event-fx
+ ::init
+ (fn [_ [_ reg-key file]]
+   {:dispatch [::update-editor-text reg-key file]}))
 
 (re-frame/reg-event-fx
  ::text-change
  [(re-frame/inject-cofx :ui-db)]
  (fn [{:keys [ui-db]} [_ reg-key {:keys [db/id] :as file} e]]
-   (let [model      (::model (ui-db->editor-info ui-db reg-key file))
+   (let [model      (ui-db->editor ui-db reg-key file :model)
          edit-event (parse-content-event model e)]
      {:dispatch [::vcs.handlers/add-edit-event id edit-event]})))
 
@@ -132,7 +200,7 @@
  ::selection-change
  [(re-frame/inject-cofx :ui-db)]
  (fn [{:keys [ui-db]} [_ reg-key {:keys [db/id] :as file} e]]
-   (let [model      (::model (ui-db->editor-info ui-db reg-key file))
+   (let [model      (ui-db->editor ui-db reg-key file :model)
          edit-event (parse-selection-event model e)]
      {:dispatch [::vcs.handlers/add-edit-event id edit-event]})))
 
