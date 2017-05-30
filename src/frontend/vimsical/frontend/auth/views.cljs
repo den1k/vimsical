4879208797e918fd9bb2de4e1a6462e921c2eaa2
den1k <kalavox@gmail.com>
2017-05-29 20:28:54 -0400

    WIP signup component

--- src/frontend/vimsical/frontend/auth/views.cljs
+++ src/frontend/vimsical/frontend/auth/views.cljs
@@ -3,7 +3,56 @@
             [reagent.core :as reagent]
             [vimsical.frontend.util.dom :as util.dom]
             [vimsical.frontend.util.dom :refer-macros [e> e-> e->>]]
-            [vimsical.frontend.views.popovers :as popovers]))
+            [vimsical.frontend.views.popovers :as popovers]
+            [goog.string :as gstr]))
+
+(defn signup []
+  (let [status    nil
+        state     (reagent/atom {})
+        on-change (fn [k] (e> (swap! state assoc k target)))]
+    (fn []
+      (let [{:keys [first-name last-name email password]} @state]
+        [:div.signup
+         [:.beta-signup
+          "Private Beta Signup"]
+         [:form.form
+          {:on-submit (fn [e]
+                        (.preventDefault e)
+                        (when (.. e -target .checkValidity)
+                          (prn :SIGNUP-ME-UP)))}
+          [:.first-last
+           [:input.first
+            {:class         "first"
+             :type          "text"
+             :name          "name"
+             :placeholder   "First Name"
+             :auto-complete "given-name"
+             :on-change     (on-change :first-name)}]
+           [:input.last
+            {:type          "text"
+             :name          "name"
+             :placeholder   "Last Name"
+             :auto-complete "family-name"
+             :on-change     (on-change :last-name)}]]
+          [:input
+           {:type          "email"
+            :name          "email"
+            :placeholder   "Email"
+            :auto-complete "email"
+            :on-change     (on-change :email)}]
+          [:input
+           {:type          "password"
+            :name          "password"
+            :placeholder   "Password"
+            :auto-complete "new-password"
+            :min-length    8
+            :title         "Please use at least 8 characters"
+            :on-change     (on-change :password)}]
+          [:input.input-button.signup-button
+           {:type  "submit"
+            :value (case status
+                     :status/pending "Signing you up..."
+                     "Sign up")}]]]))))
 
 (defn login []
   [:div.auth.login
