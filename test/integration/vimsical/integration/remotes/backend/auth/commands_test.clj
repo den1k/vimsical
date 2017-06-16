cb82e37c1ee25e3fa7b8d5d19eb1f3c2f163975f
Julien Fantin <julienfantin@gmail.com>
2017-06-16 15:18:16 -0400

    invite-signup integration test

--- test/integration/vimsical/integration/remotes/backend/auth/commands_test.clj
+++ test/integration/vimsical/integration/remotes/backend/auth/commands_test.clj
@@ -1,7 +1,7 @@
 (ns vimsical.integration.remotes.backend.auth.commands-test
   (:require
    [clojure.spec.alpha :as s]
-   [clojure.test :refer [deftest is use-fixtures]]
+   [clojure.test :refer [deftest is use-fixtures testing]]
    [com.stuartsierra.mapgraph :as mg]
    [day8.re-frame.test :as re-frame.test]
    [orchestra.spec.test :as st]
@@ -12,6 +12,7 @@
    [vimsical.frontend.db :as db]
    [vimsical.frontend.remotes.fx :as frontend.remotes.fx]
    [vimsical.queries.user :as queries.user]
+   [vimsical.backend.handlers.auth.commands :refer [create-invite!]]
    [vimsical.user :as user]))
 
 (st/instrument)
@@ -42,6 +43,27 @@
    (mg/pull
     @db-sub
     [{[:app/user '_] queries.user/pull-query}])))
+
+(deftest invite-signup-test
+  (testing "Signup with token"
+    (let [token       (create-invite! (:datomic system.fixture/*system*) "foo" "bar")
+          status-key  (uuid)
+          signup-user {:db/uid (uuid :signup-user)
+                       ::user/first-name "foo"
+                       ::user/last-name  "bar"
+                       ::user/email      "foo@bar.com"
+                       ::user/password   "foobar"}
+          status-sub  (re-frame/subscribe [::frontend.remotes.fx/status :backend status-key])
+          db-sub      (re-frame/subscribe [::db/db])]
+      (re-frame/dispatch [::frontend.auth.handlers/invite-signup token signup-user status-key])
+      (is (= ::frontend.remotes.fx/success @status-sub))
+      (let [app-user (get-app-user db-sub)]
+        (is (s/valid? ::user/user app-user))
+        (is (= "foo" (::user/first-name app-user)))
+        (is (= (uuid :signup-user) (:db/uid app-user))))
+      (testing "Token expired"
+        (re-frame/dispatch [::frontend.auth.handlers/invite-signup token signup-user status-key])
+        (is (s/valid? ::frontend.remotes.fx/error @status-sub))))))
 
 (deftest signup-test
   (let [status-key    (uuid)
