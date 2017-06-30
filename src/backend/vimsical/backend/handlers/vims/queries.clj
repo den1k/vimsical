e249a87bf95ae307d1f0e24ff5903f6a047859cb
Julien Fantin <julienfantin@gmail.com>
2017-07-01 00:17:37 +0200

    Add vims snapshot to the remote vims query
    This ensures that we get snapshots when querying for a single vims
    
    Impl notes:
    
    - Due to the primary key for snapshots in cassandra we need to wait for the
      vims query from datomic before we can fetch the snapshots
    - There is some duplication with the user quey in the snapshots join logic
    - We get the snapshots for the vims owner

--- src/backend/vimsical/backend/handlers/vims/queries.clj
+++ src/backend/vimsical/backend/handlers/vims/queries.clj
@@ -1,22 +1,48 @@
 (ns vimsical.backend.handlers.vims.queries
   (:require
+   [clojure.core.async :as a]
    [clojure.spec.alpha :as s]
    [vimsical.backend.components.datomic :as datomic]
    [vimsical.backend.components.delta-store :as delta-store]
    [vimsical.backend.components.delta-store.protocol :as delta-store.protocol]
+   [vimsical.backend.components.snapshot-store.protocol :as snapshot-store.protocol]
    [vimsical.backend.handlers.multi :as multi]
+   [vimsical.backend.util.async :as async :refer [<?]]
+   [vimsical.common.uuid :refer [uuid]]
    [vimsical.queries.vims :as queries.vims]
-   [vimsical.remotes.backend.vims.queries :as queries]))
+   [vimsical.remotes.backend.vims.queries :as queries]
+   [vimsical.vcs.branch :as vcs.branch]
+   [vimsical.vcs.snapshot :as snapshot]
+   [vimsical.vims :as vims]))
 
 ;;
 ;; * Vims
 ;;
 
+;; XXX Duplication from vimsical.backend.handlers.user.queries
+(defn- vims+snapshots-chan
+  [snapshot-store uuid-fn user-uid {vims-uid :db/uid :as vims}]
+  (a/go
+    (let [snapshots-chan           (snapshot-store.protocol/select-vims-snapshots-chan snapshot-store user-uid vims-uid)
+          snapshots                (a/<! (a/into [] snapshots-chan))
+          snapshots-by-file-uid    (group-by ::snapshot/file-uid snapshots)
+          vims-master-branch-files (-> vims vims/master-branch ::vcs.branch/files)]
+      (letfn [(file->frontend-snapshot [{file-uid :db/uid :as file}]
+                (when-some [[raw-snapshot] (get snapshots-by-file-uid file-uid)]
+                  (snapshot/->frontend-snapshot (uuid-fn) raw-snapshot file)))]
+        (assoc vims ::vims/snapshots
+               (into [] (keep file->frontend-snapshot) vims-master-branch-files))))))
+
 (defmethod multi/context-spec ::queries/vims [_] (s/keys :req-un [::datomic/datomic]))
 (defmethod multi/handle-event ::queries/vims
-  [{:keys [datomic] :as context} [_ vims-uid]]
+  [{:keys [datomic snapshot-store] :as context} [_ vims-uid]]
   (if-some [vims (datomic/pull datomic queries.vims/pull-query [:db/uid vims-uid])]
-    (multi/set-response context vims)
+    ;; NOTE querying snapshots of the vims owner, this will need some
+    ;; refinements once we have forks
+    (let [owner-uid (-> vims ::vims/owner :db/uid)]
+      (multi/async
+       context
+       (multi/set-response context (<? (vims+snapshots-chan snapshot-store uuid owner-uid vims)))))
     (multi/set-response context 404 nil)))
 
 ;;
