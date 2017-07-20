8d535261ee8c0286a3f9b2f03c1a59b42aa37879
Julien Fantin <julienfantin@gmail.com>
2017-07-20 16:13:31 -0400

    Don't include stacktraces in prod error responses

--- src/backend/vimsical/backend/components/server/interceptors/errors.clj
+++ src/backend/vimsical/backend/components/server/interceptors/errors.clj
@@ -1,9 +1,12 @@
 (ns vimsical.backend.components.server.interceptors.errors
   (:require
-   [ring.util.response :as response]
    [clojure.pprint :as pprint]
    [io.pedestal.interceptor :as interceptor]
-   [vimsical.backend.util.log :as log]))
+   [ring.util.response :as response]
+   [vimsical.backend.util.log :as log]
+   [vimsical.common.env :as env]))
+
+(def debug? (not= :prod (env/env)))
 
 (defn- error-debug
   [context exception]
@@ -11,13 +14,15 @@
    {:msg       "Error interceptor caught an exception; Forwarding it as the response."
     :exception exception})
   (assoc context :response
-         (-> (response/response
-              (with-out-str (println "Error processing request!")
-                (println "Exception:\n")
-                (pprint/pprint exception)
-                (println "\nContext:\n")
-                (pprint/pprint context)))
-             (response/status 500))))
+         (response/status
+          (when debug?
+            (response/response
+             (with-out-str (println "Error processing request!")
+               (println "Exception:\n")
+               (pprint/pprint exception)
+               (println "\nContext:\n")
+               (pprint/pprint context))))
+          500)))
 
 (def debug
   (interceptor/interceptor
